7942c41628bc298cd15155386298f1c9fc8cc7bb
Andrew McIntosh <andrew.mcintosh@promisepay.com>
2015-10-29 13:00:11 +1100

    - User creating - Still need to configure it for your marketplace Danny

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,104 +6,105 @@ use Illuminate\Http\Request;
 use App\User;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-require '../vendor/promisepay/promisepay-php/init.php';
+use PromisePay;
 
-class UsersController extends Controller {
+class UsersController extends Controller
+{
 
-  /**
-   * Display a listing of the resource.
-   *
-   * @return Response
-   */
-  public function index()
-  {
-    $users = User::all();
-  }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $users = User::all();
+    }
 
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return Response
-   */
-  public function create(Request $request)
-  {
-    $first_name = $request->input('first_name');
-    $last_name = $request->input('last_name');
-    $email = $request->input('email');
-    $password = $request->input('password');
-    $address_line1 = $request->input('address_line1');
-    $state = $request->input('state');
-    $city = $request->input('city');
-    $zip = $request->input('zip');
-    $country = $request->input('country');
-    $dob = $request->input('dob');
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create(Request $request)
+    {
+        $first_name = $request->input('first_name');
+        $last_name = $request->input('last_name');
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $address_line1 = $request->input('address_line1');
+        $state = $request->input('state');
+        $city = $request->input('city');
+        $zip = $request->input('zip');
+        $country = $request->input('country');
+        $dob = $request->input('dob');
 
-    $repo = new PromisePay::UserRepository();
-    $user = new User;
-    $user->first_name = $first_name;
-    $user->last_name = $last_name;
-    $user->email = $email;
-    $user->password = $password;
-    $user->address_line1 = $address_line1;
-    $user->state = $state;
-    $user->city = $city;
-    $user->zip = $zip;
-    $user->country = $country;
-    $user->dob = $dob;
-    $repo->createUser($user);
-    $user->save();
+        $repo = new PromisePay\UserRepository();
+        $user = new PromisePay\DataObjects\User($arr = array(
+            'id' => (string)mt_rand(),
+            'email' => $email,
+            'first_name' => $first_name,
+            'last_name' => $last_name,
+            'address_line1' => $address_line1,
+            'state' => $state,
+            'city' => $city,
+            'zip' => $zip,
+            'country' => $country,
+            'dob' => $dob
+        ));
 
+        $repo->createUser($user);
 
-    return redirect('/');
-  }
+        return redirect('/');
+    }
 
-  public function store()
-  {
-    //
-  }
+    public function store()
+    {
+        //
+    }
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function show($id)
-  {
-    //
-  }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
 
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function edit($id)
-  {
-    //
-  }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
 
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function update($id)
-  {
-    //
-  }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function destroy($id)
-  {
-    //
-  }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 
 }
